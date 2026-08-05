03fd509e44856664c187dab3dc58f32a7d6b1767
IrfanAli219 <irfan14050@gmail.com>
2026-08-05 21:32:12 +0500

    interpolation testing added

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from utils.parser import (
     extract_launch_lines,
     create_dataframe
 )
+from utils.interpolation import create_height_grid
+from utils.interpolation import interpolate_profile
+
 
 
 # ==================================================
@@ -108,8 +111,33 @@ raw_data = extract_launch_lines(
 
 df = create_dataframe(raw_data)
 
+print("\nOriginal Profile")
 print(df.head())
 
+df = interpolate_profile(df)
+
+print("\nInterpolated Profile")
+print(df.head())
+quit()
+# ==========================================================
+# Test Height Grid
+# ==========================================================
+
+print("\nStep 5 : Creating Height Grid...")
+
+height_grid = create_height_grid(
+    min_height=df["HGHT"].min(),
+    max_height=df["HGHT"].max(),
+    step=100
+)
+
+print("\nMinimum Height :", df["HGHT"].min())
+print("Maximum Height :", df["HGHT"].max())
+
+print("\nHeight Grid")
+print(height_grid)
+
+print(f"\nTotal Grid Levels : {len(height_grid)}")
 # ==========================================================
 # Remove Missing Values
 # ==========================================================
